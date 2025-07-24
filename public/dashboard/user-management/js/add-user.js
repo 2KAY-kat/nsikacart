dfe76ac6119587f7954ae5029285073049ef5846
Kelvin 2KAY Katoya <dambwedesigns@gmail.com>
2025-07-25 01:09:30 +0200

    styling add user page for consistancy acroos and yeah its elegant ... but cant write the backend and the api stuff am tired wanna sleep

---
+++ public/dashboard/user-management/js/add-user.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('addUserForm');
+    if (!form) return;
+
+    form.addEventListener('submit', async function (e) {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData(form).entries());
+
+        const response =  await fetch('/nsikacart/api/admin/add-user.php', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+
+        const json = await response.json();
+        const feedback = document.getElementById('message');
+
+        if (json.success) {
+            feedback.textContent = 'User add seccessfully';
+            form.reset();
+        } else {
+            feedback.textContent = `${json.message || 'Failed to add user'}`;
+        }
+    });
+});
